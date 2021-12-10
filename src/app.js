5de2457bc588022f716e53cc08f89e005b160399
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2021-12-10 13:15:04 -0600

    This commit adds functionality for rendering the current weather in html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const APIKey = '176e29a31996dac45b61c17713b398de';
-
-let userLong;
-let userLat;
+const APICall = 'https://api.openweathermap.org/data/2.5/'
 
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(position => {
-    userLong = position.coords.longitude;
-    userLat = position.coords.latitude;
+    let userLong = position.coords.longitude;
+    let userLat = position.coords.latitude;
+    return new Promise((resolve) => {
+      resolve(renderCurrentWeather(userLong, userLat));
+    });
   });
+
 } else {
   alert(`Can't find your location!`);
+}
+
+function renderCurrentWeather(long, lat) {
+  const currentWeather = document.querySelector('.current-conditions');
+
+  while (currentWeather.firstChild) {
+    currentWeather.removeChild(currentWeather.firstChild);
+  }
+
+  return fetch(`${APICall}weather?lat=${lat}&lon=${long}&units=metric&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(data => {
+      currentWeather.insertAdjacentHTML('afterbegin', `
+        <h2>Current Conditions</h2>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+        <div class="current">
+          <div class="temp">${data.main.temp}&#176;C</div>
+          <div class="condition">${data.weather[0].description}</div>
+        </div>`);
+    });
 }
